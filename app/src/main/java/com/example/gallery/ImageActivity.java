7001f55367e58090da18a9647d5fc003dbe712fb
Цомкалов Игорь <igorcomkalov@gmail.com>
2023-06-10 03:27:03 +0300

    екук

---
+++ app/src/main/java/com/example/gallery/ImageActivity.java
@@ -0,0 +1,28 @@
+package com.example.gallery;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+
+        String imgPath = getIntent().getStringExtra("imgPath");
+        ImageView imageView = findViewById(R.id.idImage);
+
+        File imgFile = new File(imgPath);
+
+        if (imgFile.exists()) {
+            Picasso.get().load(imgFile).placeholder(R.drawable.ic_launcher_background).into(imageView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gallery/MyAdapter.java
@@ -0,0 +1,91 @@
+package com.example.gallery;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private ArrayList<Cell> galleryList;
+    private Context context;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(Context context, ArrayList<Cell> galleryList) {
+        this.context = context;
+        this.galleryList = galleryList;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                         int viewType) {
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cell, parent, false);
+        return new ViewHolder(view);
+    }
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, @SuppressLint("RecyclerView") final int position) {
+        // - get element from your dataset at this position
+        File imgFile = new File(galleryList.get(position).getPath());
+        // - replace the contents of the view with that element
+        viewHolder.img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        setImageFromPath(galleryList.get(position).getPath(), viewHolder.img);
+        viewHolder.img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // inside on click listener we are creating a new intent
+                Intent i = new Intent(context, ImageActivity.class);
+
+                // on below line we are passing the image path to our new activity.
+                i.putExtra("imgPath", galleryList.get(position).getPath());
+
+                // at last we are starting our activity.
+                context.startActivity(i);
+                //TODO что-то может происходить, если кликнуть на изображение
+                Toast.makeText(context, "" + galleryList.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public ImageView img;
+
+        public ViewHolder(View view) {
+            super(view);
+            img = view.findViewById(R.id.img);
+        }
+    }
+
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return galleryList.size();
+    }
+
+    private void setImageFromPath(String path, ImageView image) {
+        File imgFile = new File(path);
+        if (imgFile.exists()) {
+            Bitmap myBitmap = ImageHelper.decodeSampleBitmapFromPath(imgFile.getAbsolutePath(), 200, 200);
+            image.setImageBitmap(myBitmap);
+        }
+    }
+}
